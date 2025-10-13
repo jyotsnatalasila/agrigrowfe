6e54b9de30c5263aedaa1427f1745f61c7c0047c
jyotsnatalasila <jyotsnatalasila@gmail.com>
2025-10-13 21:00:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,10 +36,17 @@ import Product from './Components/Product';
 import ProductPage from './Components/ProductPage';
 import Nuts from './Components/Nuts';
 
-function App() {
+function App() 
+  const getBasename = () => {
+    if (window.location.pathname.startsWith('/agrigrowfe')) {
+      return '/agrigrowfe';
+    }
+    return '/';
+  };
+
   return (
     <LocationProvider> 
-      <Router>
+      <Router basename={getBasename()}>
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<Welcome />} />
@@ -76,6 +83,7 @@ function App() {
             <Route path="/aquaticfeed" element={<AquaticFeedPage />} />
             <Route path="/nuts" element={<Nuts />} />
           </Route>
+          <Route path="*" element={<Welcome />} />
         </Routes>
       </Router>
     </LocationProvider>
